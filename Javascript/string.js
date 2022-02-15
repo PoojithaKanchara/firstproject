67130ac42ff95db73bbf63092b17315f75bd5755
John Doe <johndoe@email.com>
2022-02-15 12:27:53 +0530

    StringProgram

---
+++ Javascript/string.js
@@ -0,0 +1,29 @@
+let str = 'Poojitha chandra reddy';
+
+
+console.log('length: '+ str.length);
+console.log(" index: "+ str.indexOf('j'))
+console.log(" index: "+ str.toUpperCase())
+
+
+let stAry= str.split('').reverse().join('');
+
+console.log("reverse : "+ stAry);
+
+str.toLocaleLowerCase();
+
+
+console.log(stAry);
+
+
+let a =[1,3,4,5,2]
+
+console.log("Index of : "+a.indexOf(4))
+console.log("Include of : "+a.slice(0, 2));
+
+console.log(Math.max(1,3,4));
+console.log(Math.pow(4,2))
+
+console.log(a.reverse())
+
+
