779eb7fdc4e2d20a3111eecf2cfae95184fca4c0
K Chandrasekhar Reddy <k.chandrasekhar.reddy@sap.com>
2022-02-15 20:44:55 +0530

    debugging session

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,1 +1,22 @@
-console.log("we fuck each other")
+
+function fuck1(a, b){
+
+    let r = a*b;
+    console.log('TO fuck result'+r)
+}
+
+function fuck2(a, b){
+
+    let r = a*b;
+    console.log('TO fuck2 result'+r)
+}
+
+
+
+fuck1(10,20)
+
+console.log('Main Fucker');
+
+fuck1(100,2)
+
+setTimeout(fuck2(2,3), 20000);
