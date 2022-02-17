ecb83fc26495ccbf89b091e5e64fe9b46a4e15cd
John Doe <johndoe@email.com>
2022-02-17 20:56:32 +0530

    export import practise

---
+++ Javascript/exportpract.js
@@ -0,0 +1,14 @@
+function add(){
+    return a+b;
+    console.log("add ")
+}
+
+function sub(){
+    return a-b;
+    console.log("sub")
+}
+
+module.exports={
+    add,
+    sub,
+}
---
+++ Javascript/importpract.js
@@ -0,0 +1,6 @@
+let f = require('./exportpract');
+
+console.log(f.add(2,3));
+console.log(f.sub(3,2));
+
+
